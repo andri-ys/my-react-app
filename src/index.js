dd163972e5fcebf54f91d172ef4aa36dddc935bf
andri-ys <andri.ys.st@gmail.com>
2019-08-17 20:49:01 +0700

    render the squares using loop instead of hardcode

--- src/index.js
+++ src/index.js
@@ -23,23 +23,23 @@ class Board extends React.Component {
     }
 
     render() {
+        let rows = [];
+
+        for (let i = 0; i < 3; i++){
+            let squares = [];
+            for (let j = 0; j < 3; j++){
+                squares.push(this.renderSquare((3 * i) + j));
+            }
+            rows.push(
+                <div className="board-row">
+                    {squares}
+                </div> 
+            );
+        }
+
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {rows}
             </div>
         );
     }
@@ -107,7 +107,7 @@ class Game extends React.Component {
             return (
                 <li key={index}>
                     <button 
-                        class={this.state.stepNumber == index ? "selected" : ""} 
+                        className={this.state.stepNumber == index ? "selected" : ""} 
                         onClick={() => this.jumpTo(index)}>{description}
                     </button>
                 </li>
